b0fc8dd24ad91e0152aadc288c290a20eef78895
suntoes <mar26xd@gmail.com>
2022-03-04 21:17:09 +0800

    exec ts w/ node via `npm run ts-start`

--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.port || 3000;
-app.use(express.static('build'));
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
